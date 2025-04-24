aa46f281e987305a5470d59979a735328c07e0ad
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2025-04-24 18:58:42 +0600

    Add files via upload

---
+++ Regular_solve/Day9/B. Mahmoud and a Triangle.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin>>n;
+
+    vector<int> v(n);
+    for(int i=0; i<n; i++)
+    {
+        cin>>v[i];
+    }
+
+    sort(v.begin(),v.end());
+
+    for(int i=1; i<n-1; i++)
+    {
+        if(v[i-1]+v[i]>v[i+1])
+        {
+            cout<<"YES"<<endl;
+            return 0;
+        }
+    }
+
+    cout<<"NO"<<endl;
+}
