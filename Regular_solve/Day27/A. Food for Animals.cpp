fdb054da4cd02a6350ff313f7fa4c6c218e68c66
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2025-05-15 20:40:41 +0600

    Add files via upload

---
+++ Regular_solve/Day27/A. Food for Animals.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n >> m;
+
+    int result = -1;
+
+
+    if (m % n == 0)
+    {
+        result = 0;
+        int d = m / n;
+
+
+        while (d % 2 == 0)
+        {
+            d /= 2;
+            result++;
+        }
+
+
+        while (d % 3 == 0)
+        {
+            d /= 3;
+            result++;
+        }
+
+        if (d != 1)
+        {
+            result = -1;
+        }
+    }
+
+    cout << result << endl;
+    return 0;
+}
