2d9c87469511b1db94d64a7eaef0b6b879ef68c3
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2025-06-26 19:52:33 +0600

    Add files via upload

---
+++ Regular_solve/june_26/Microsoft Defender.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void solve()
+{
+    int n,m;
+    cin>>n>>m;
+
+    cout<<((n*m)+1)/2<<'\n';
+}
+
+int main()
+{
+    int t;
+    cin>>t;
+
+    while(t--)
+    {
+        solve();
+    }
+}
