be3333e5fd6b2a4ffb32502535d7c640f47f7474
Moinul Islam Sajid <127509739+moinulislamsajid@users.noreply.github.com>
2025-12-06 20:34:39 +0600

    Add files via upload

--- Regular_solve/Nov_30/A. Shovels and Swords.cpp
+++ Regular_solve/Nov_30/A. Shovels and Swords.cpp
@@ -2,13 +2,15 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int main() {
+int main()
+{
     ios::sync_with_stdio(false);
     cin.tie(nullptr);
 
     int t;
     cin >> t;
-    while (t--) {
+    while (t--)
+    {
         long long l, r;
         cin >> l >> r;
         cout << min({l, r, (l + r) / 3}) << "\n";
